6bc3b73fde0c8bc239db372773b2845a5fc6c033
imshoeb8 <imshoeb8@gmail.com>
2024-09-12 06:42:25 +0000

    Code -2

--- collataz-conjecture.py
+++ collataz-conjecture.py
@@ -3,3 +3,17 @@ def steps(number):
     if number == 1: return 0
     number = number / 2 if number % 2 == 0 else number * 3 + 1
     return 1 + steps(number)
+
+def steps(n):
+    if n < 1:
+        raise ValueError('Only positive integers are allowed')
+
+    steps = 0
+
+    while n > 1 :
+        n = 3 * n + 1 if n % 2 else n / 2
+        steps += 1
+
+
+    return steps
+    
