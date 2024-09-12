9e9060ab97002e8cad45fa0b1c2c3a757fb90b5b
imshoeb8 <imshoeb8@gmail.com>
2024-09-12 06:31:54 +0000

    Even or ODD Numbers

---
+++ collataz-conjecture.py
@@ -0,0 +1,5 @@
+def steps(number):
+    if number <= 0: raise ValueError("Only positive integers are allowed")
+    if number == 1: return 0
+    number = number / 2 if number % 2 == 0 else number * 3 + 1
+    return 1 + steps(number)
