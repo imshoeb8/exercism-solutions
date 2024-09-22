6048824cd611620a87b48087b9c5bfa1ac8b718c
imshoeb8 <imshoeb8@gmail.com>
2024-09-22 17:37:12 +0600

    sound of drop

---
+++ raindrop.py
@@ -0,0 +1,11 @@
+def convert(number):
+    sound = ''
+    
+    if number % 3 == 0:
+        sound += 'Pling'
+    if number % 5 == 0:
+        sound += 'Plang'
+    if number % 7 == 0:
+        sound += 'Plong'
+        
+    return sound if sound else str(number)
